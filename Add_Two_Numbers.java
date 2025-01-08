2f20577eb3c977d396cc0e092c465580f8622aeb
Niraj Kumar <nirajgupta7481@gmail.com>
2025-01-08 10:54:07 +0530

    SecondProblemAddedSuccessfully

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Add_Two_Numbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        
+        double sum = num1 + num2;
+        
+        System.out.println("The sum is: " + sum);
+        
+    }
+}
