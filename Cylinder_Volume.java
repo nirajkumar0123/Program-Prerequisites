d96614934397897ab56a83feaa0062215a088654
Niraj Kumar <nirajgupta7481@gmail.com>
2025-01-08 10:59:09 +0530

    FifthProblemAddedSuccessfully

---
+++ Cylinder_Volume.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Cylinder_Volume {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+        
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+        
+        double volume = Math.PI * Math.pow(radius, 2) * height;
+        
+        System.out.println("The volume of the cylinder is: " + volume);
+        
+    
+    }
+}
